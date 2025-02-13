169bd1dad81f2dfaeeee69faa48db593220b8eeb
Attorney Valois <v.niyigaba@alustudent.com>
2025-02-13 15:00:33 +0200

    Update 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 
-for i in range(25, -1, -1):
-    char = chr(ord('a') + i) if i % 2 == 0 else chr(ord('A') + i)
-    print(char, end='')
-
-print()
+print("".join(chr(ord('a') + i) if i % 2 == 0 else chr(ord('A') + i) for i in range(25, -1, -1)))

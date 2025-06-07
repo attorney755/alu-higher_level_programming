2025638cdced2ded7c1ccc9de9023e1520fc5996
attorney755 <v.niyigaba@alustudent.com>
2025-06-07 20:15:51 +0200

    All

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
+const file = process.argv.slice(2)[0];
 
-fs.readFile(filePath, 'utf8', (error, data) => {
-  if (error) {
-    console.error(error);
-    return;
+fs.readFile(file, 'utf8', (err, contents) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(contents);
   }
-
-  console.log(data);
 });
